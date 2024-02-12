b97616ab9ef83338d50a7142decfcb1acb629593
DarianOthman <darian.othman@gmail.com>
2024-02-12 13:37:41 +0100

    Creation of Functions - Part 06

--- File_Opening.py
+++ File_Opening.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 from matplotlib_venn import venn3
 import ast
 import networkx as nx
+
+
 def read_data(file_path):
     # Read in Instagram data
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -25,7 +27,6 @@ def read_data(file_path):
 
 
 def get_unique_values(dataframe, column_name):
-
     # Convert values in the specified column to uppercase and remove spaces
     dataframe[column_name] = dataframe[column_name].apply(lambda x: x.replace(" ", "").upper())
 
@@ -63,11 +64,13 @@ def find_common_elements_and_plot(set_1, set_2, set_3, name_1, name_2, name_3, t
     plt.title(title)
     plt.show()
 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_instances_by_week(dataframe, date_column, title='Evolution of Instances by Week', x_label='Date', y_label='Number of Instances', rotation=45):
 
+def plot_instances_by_week(dataframe, date_column, title='Evolution of Instances by Week', x_label='Date',
+                           y_label='Number of Instances', rotation=45):
     # Convert date column to datetime format
     dataframe[date_column] = pd.to_datetime(dataframe[date_column])
 
@@ -85,8 +88,9 @@ def plot_instances_by_week(dataframe, date_column, title='Evolution of Instances
 
     # Show the plot
     plt.show()
-    
-def plot_instances_together(df1,df2,df3,timecol1,timecol2,timecol3,label1,label2,label3,title):
+
+
+def plot_instances_together(df1, df2, df3, timecol1, timecol2, timecol3, label1, label2, label3, title):
     # Convert to datetime format for each DataFrame
     df1[timecol1] = pd.to_datetime(df1[timecol1])
     df2[timecol2] = pd.to_datetime(df2[timecol2])
@@ -116,7 +120,6 @@ def plot_instances_together(df1,df2,df3,timecol1,timecol2,timecol3,label1,label2
 
 
 def calculate_time_between_posts(dataframe, username_column, date_column):
-
     # Get unique usernames
     unique_usernames = dataframe[username_column].unique()
 
@@ -132,8 +135,8 @@ def calculate_time_between_posts(dataframe, username_column, date_column):
 
     return timediff_df
 
-def plot_time_between_posts(df1, df2, df3, label1, label2, label3, title):
 
+def plot_time_between_posts(df1, df2, df3, label1, label2, label3, title):
     # Combine the data into a list of arrays
     data_to_plot = [
         df1["Time Difference"].dt.total_seconds().dropna() / (60 * 60 * 24),
@@ -156,23 +159,50 @@ def plot_time_between_posts(df1, df2, df3, label1, label2, label3, title):
     plt.show()
 
 
-def draw_hashtag_network(dataframe, hashtags_column, title, k_value=0.2, node_size=2, edge_color='grey', width=0.1, alpha=0.7):
-
-
+def draw_hashtag_network(dataframe, hashtags_column, title, sample_size=1000, k_value=0.2, node_size=2,
+                         edge_color='grey', width=0.1, alpha=0.7):
     # Convert the column containing lists of hashtags to lists
     dataframe[hashtags_column] = dataframe[hashtags_column].apply(lambda x: ast.literal_eval(x) if x else "")
 
     # Filter out rows with non-empty lists of hashtags
     filtered_df = dataframe[dataframe[hashtags_column].apply(lambda x: bool(x))].reset_index(drop=True)
 
-    # Create a graph
-    G = nx.Graph()
+    sample_size = sample_size
+    random_sample = filtered_df.sample(n=sample_size, random_state=42)
 
-    # Add edges to the graph
-    for words_list in filtered_df[hashtags_column]:
-        G.add_edges_from([(word1, word2) for i, word1 in enumerate(words_list) for j, word2 in enumerate(words_list) if i < j])
+    G = nx.Graph()
+    for words_list in random_sample[hashtags_column]:
+        G.add_edges_from(
+            [(word1, word2) for i, word1 in enumerate(words_list) for j, word2 in enumerate(words_list) if i < j])
 
     # Calculate the spring layout
+    pos = nx.spring_layout(G, k=k_value)
+
+    # Draw the graph with specified layout
+    nx.draw(G, pos, with_labels=False, node_size=node_size, edge_color=edge_color, width=width, alpha=alpha)
+    plt.title(title)
+    plt.show()
+
+
+def draw_tag_network(dataframe, tags_column, title, sample_size=1000, k_value=0.2, node_size=2, edge_color='grey',
+                     width=0.1, alpha=0.7):
+    dataframe[tags_column] = dataframe[tags_column].apply(lambda x: ast.literal_eval(x) if x else "")
+    filtered_df = dataframe[dataframe[tags_column].apply(lambda x: bool(x))].reset_index(drop=True)
+
+    sample_size = sample_size
+    random_sample = filtered_df.sample(n=sample_size, random_state=42)
+
+    G = nx.Graph()
+    # Assuming tags_column is a DataFrame column containing lists of words in each row
+    for i, row1 in random_sample.iterrows():
+        for j, row2 in random_sample.iterrows():
+            if i < j:
+                # Check if there is any common word between the two rows
+                common_words = set(row1[tags_column]) & set(row2[tags_column])
+                if common_words:
+                    G.add_edge(i, j, common_words=list(common_words))
+
+    k_value = k_value  # You can adjust this value to control the repulsion
     pos = nx.spring_layout(G, k=k_value)
 
     # Draw the graph with specified layout
