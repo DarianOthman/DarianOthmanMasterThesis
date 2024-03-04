bc4b8e7bb730f83abfc7775598bac01124d06f09
DarianOthman <darian.othman@gmail.com>
2024-03-04 11:54:16 +0100

    Weights on Networks

--- Functions/Influencer_Characteristics.py
+++ Functions/Influencer_Characteristics.py
@@ -125,8 +125,14 @@ def plot_time_between_posts(df1, df2, df3, label1, label2, label3, title,lower_l
     plt.show()
 
 
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
 def draw_hashtag_network(dataframe, hashtags_column, title, sample_size=None, k_value=0.2, node_size=2,
-                         edge_color='grey', width=0.1, alpha=0.7):
+                         edge_color='grey', alpha=0.7, edge_weight=0.1):
     dataframes = pd.DataFrame()
     # Convert the column containing lists of hashtags to lists
     dataframes[hashtags_column] = dataframe[hashtags_column].apply(
@@ -141,20 +147,25 @@ def draw_hashtag_network(dataframe, hashtags_column, title, sample_size=None, k_
 
     G = nx.Graph()
     for words_list in random_sample[hashtags_column]:
-        G.add_edges_from(
+        co_occurrences = Counter(
             [(word1, word2) for i, word1 in enumerate(words_list) for j, word2 in enumerate(words_list) if i < j])
+        for edge, weight in co_occurrences.items():
+            G.add_edge(edge[0], edge[1], weight=weight)
 
     # Calculate the spring layout
     pos = nx.spring_layout(G, k=k_value)
 
-    # Draw the graph with specified layout
-    nx.draw(G, pos, with_labels=False, node_size=node_size, edge_color=edge_color, width=width, alpha=alpha)
+    # Draw the graph with specified layout, considering edge weights
+    edge_weights = [G[u][v]['weight'] for u, v in G.edges()]
+    nx.draw(G, pos, with_labels=False, node_size=node_size, edge_color=edge_color,
+            width=[weight * edge_weight for weight in edge_weights], alpha=alpha)
+
     plt.title(title)
     plt.show()
 
 
 def draw_tag_network(dataframe, tags_column, title, sample_size=None, k_value=0.5, node_size=2,
-                     edge_color='grey', width=0.1, alpha=0.7):
+                     edge_color='grey', alpha=0.7, edge_weight=0.1):
     dataframes = pd.DataFrame()
     dataframes[tags_column] = dataframe[tags_column].apply(lambda x: [tag.strip() for tag in x.split(",")] if x else [])
     filtered_df = dataframes[dataframes[tags_column].apply(lambda x: bool(x))].reset_index(drop=True)
@@ -171,13 +182,18 @@ def draw_tag_network(dataframe, tags_column, title, sample_size=None, k_value=0.
                 # Check if there is any common word between the two rows
                 common_words = set(row1[tags_column]) & set(row2[tags_column])
                 if common_words:
-                    G.add_edge(i, j, common_words=list(common_words))
+                    co_occurrences = Counter(common_words)
+                    for word, weight in co_occurrences.items():
+                        G.add_edge(i, j, word=word, weight=weight)
 
     k_value = k_value  # You can adjust this value to control the repulsion
     pos = nx.spring_layout(G, k=k_value)
 
     # Draw the graph with specified layout
-    nx.draw(G, pos, with_labels=False, node_size=node_size, edge_color=edge_color, width=width, alpha=alpha)
+    edge_weights = [G[u][v]['weight'] for u, v in G.edges()]
+    nx.draw(G, pos, with_labels=False, node_size=node_size, edge_color=edge_color,
+            width=[weight * edge_weight for weight in edge_weights], alpha=alpha)
+
     plt.title(title)
     plt.show()
 
